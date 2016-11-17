5e43a97616eb98be9cb2a58bb23d88c217bb78b4
Justin Ng <jng1096@gmail.com>
2016-11-17 00:36:22 -0800

    More minor UI changes

--- app/src/main/java/com/momentum/digitalsignagedemo/LoginActivity.java
+++ app/src/main/java/com/momentum/digitalsignagedemo/LoginActivity.java
@@ -2,9 +2,12 @@ package com.momentum.digitalsignagedemo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import org.json.JSONArray;
@@ -26,6 +29,11 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        setTitle("Log in");
         setContentView(R.layout.login_activity);
     }
 
