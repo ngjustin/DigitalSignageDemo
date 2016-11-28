96c15fe74c62448a4227fe54b3162654e51a5671
Justin Ng <jng1096@gmail.com>
2016-11-28 09:35:48 -0800

    Began reworked implementation of QR scanner

--- app/src/main/java/com/momentum/digitalsignagedemo/SecondFragment.java
+++ app/src/main/java/com/momentum/digitalsignagedemo/SecondFragment.java
@@ -1,5 +1,6 @@
 package com.momentum.digitalsignagedemo;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * Created by Justin on 10/11/2016.
@@ -15,16 +21,62 @@ import android.widget.TextView;
 
 public class SecondFragment extends Fragment {
 
+    private String toast;
+
+    public SecondFragment() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        displayToast();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.second_frag, container, false);
         getActivity().setTitle("QR Scanner");
 
+        Button scan = (Button) v.findViewById(R.id.button);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanFromFragment();
+            }
+        });
+
         return v;
+    }
+
+    public void scanFromFragment() {
+        IntentIntegrator.forSupportFragment(this).initiateScan();
     }
 
     public static SecondFragment newInstance() {
         SecondFragment f = new SecondFragment();
         return f;
+    }
+
+    private void displayToast() {
+        if(getActivity() != null && toast != null) {
+            Toast.makeText(getActivity(), toast, Toast.LENGTH_LONG).show();
+            toast = null;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                toast = "Cancelled from fragment";
+            } else {
+                toast = "Scanned from fragment: " + result.getContents();
+                System.out.println(result.getContents());
+            }
+
+            // At this point we may or may not have a reference to the activity
+            displayToast();
+        }
     }
 }
